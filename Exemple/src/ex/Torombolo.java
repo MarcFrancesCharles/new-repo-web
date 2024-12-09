342676f93d45ca27a710b37de43553ff6a516968
MarcFrancesCharles <mfrancescharles@gmail.com>
2024-12-09 19:13:09 +0100

    Update Torombolo.java

--- Exemple/src/ex/Torombolo.java
+++ Exemple/src/ex/Torombolo.java
@@ -5,7 +5,9 @@ public class Torombolo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Hola");
+
+		System.out.println("Hola12");
+
 	}
 
 }
